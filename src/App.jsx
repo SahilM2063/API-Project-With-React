8f4b06c645c3b0b25dd34c2f238e4d6f80ce519e
SahilM2063 <makadiasahil9846@gmail.com>
2024-02-18 22:01:08 +0530

    Created Card Design

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,20 @@ function App() {
         </ul>
       </nav>
 
-      <div className="h-full w-[85%] bg-slate-500"></div>
+      <div className="h-full w-[85%] grid grid-cols-5 gap-10 p-4">
+        <div className="group card flex flex-col max-h-[36vh] p-4 bg-white border-[1px] cursor-pointer">
+          <div className="img-container w-full h-[80%] ">
+            <img
+              className="w-full h-full object-contain group-hover:scale-105 transition-all"
+              src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+              alt="product"
+            />
+          </div>
+          <div className="title w-full h-[20%] py-2">
+            Lorem ipsum dolor sit.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
